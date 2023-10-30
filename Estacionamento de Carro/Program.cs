69631e9e1f8b6d253519faa04fb92cd91991f8e9
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-10-29 23:35:04 -0300

    pronto

--- Estacionamento de Carro/Program.cs
+++ Estacionamento de Carro/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Digite o Preço por Hora:");
 int precohora = Convert.ToInt32(Console.ReadLine());
 
 bool menu = true;
+List<string?> carros = new List<string?>();
 
 while(menu)
 {
@@ -23,13 +24,24 @@ while(menu)
     switch (opcao)
     {
         case "1":
-            
+            Console.WriteLine("Digite a placa do veiculo que estacionou:");
+            carros.Add(Console.ReadLine());
             break;
         case "2":
-            
+            Console.WriteLine("Digite a placa do veiculo que ira sair:");
+            carros.Remove(Console.ReadLine());
+
+            Console.WriteLine("Digite Quanto tempo ele ficou:");
+            int total = (Convert.ToInt32(Console.ReadLine()) * precohora) + precoinicial;
+
+            Console.WriteLine($"Ele tera que pagar {total} reais");
             break;
         case "3":
-
+            Console.WriteLine("Esses são os carros que estão no estabelecimento");
+            foreach (var item in carros)
+            {
+                Console.WriteLine(item);
+            }
             break;
         case "4":
             menu = false;
